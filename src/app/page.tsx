b4ee44372e0a5d8047b8878763c3e2693247443c
sc30gsw <haidoushi875@gmail.com>
2023-08-06 15:53:41 +0900

    記事一覧のレイアウトの作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 
+import SinglePost from '../../lib/components/Post/SinglePost'
 import { getAllPosts } from '../../lib/notionAPI'
 
 const Home = async () => {
   const posts = await getAllPosts()
 
-  return <div></div>
+  return (
+    <main className="container w-full mt-16">
+      <h1 className="text-5xl font-medium text-center mb-16">Notion Blog🚀</h1>
+      {posts.map((post: any) => (
+        <div key={post.id} className="mx-4">
+          <SinglePost
+            title={post.title}
+            description={post.description}
+            date={post.date}
+            tags={post.tags}
+            slug={post.slug}
+          />
+        </div>
+      ))}
+    </main>
+  )
 }
 
 export default Home
